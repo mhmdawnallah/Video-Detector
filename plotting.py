8dd6b4da0e943bc5db24a27c6f165ef85f954f1f
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2022-01-10 04:02:33 +0200

    Detect Movies Object, Store resulted numpyarrays of objects into csv file, Plot csv file into a quadrant form using bokeh library

---
+++ plotting.py
@@ -0,0 +1,27 @@
+from motion_detector import df
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
+
+df["Start_string"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_string"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+cds = ColumnDataSource(df)
+
+p = figure(x_axis_type="datetime",height=100,width=500,sizing_mode="scale_both")
+
+p.yaxis.minor_tick_line_color = None
+p.yaxis.ticker.desired_num_ticks = 1
+
+p.title.text="Motion Graph"
+p.title.text_color="Gray"
+p.title.text_font="times"
+p.title.text_font_style="bold"
+p.title.text_font_size = '25pt'
+p.title.align = 'center'
+
+hover = HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
+p.add_tools(hover)
+
+q = p.quad(left="Start",right="End",bottom=0,top=1,color="green",source=cds)
+output_file("Graph.html")
+show(p)
